09a113fbf5113e0f5822cd10cb7faa962246fa0b
amos zohar <azamoszohar@gmail.com>
2023-07-08 15:48:25 +0300

    finished q3 locally.if also runs on testing env, this will be the last commit

--- MainTrain.c
+++ MainTrain.c
@@ -85,74 +85,74 @@ int main()
 		printf("specialSum function is wrong for train2_3  (-8)\n");
 	}
 
- //   //Q3
-	//Person* perArr[5];
-	//Product* prodArr[8];
-	//Yad2 y2;
-	//Product** retProd;
-	//int num;
+    //Q3
+	Person* perArr[5];
+	Product* prodArr[8];
+	Yad2 y2;
+	Product** retProd;
+	int num;
 
-	//perArr[0] = createPerson("David", "32455");
-	//perArr[1] = createPerson("Moshe", "232134");
-	//perArr[2] = createPerson("Shlomi", "56454");
-	//perArr[3] = createPerson("David", "67889");
-	//perArr[4] = createPerson("Yosi", "3242234455");
+	perArr[0] = createPerson("David", "32455");
+	perArr[1] = createPerson("Moshe", "232134");
+	perArr[2] = createPerson("Shlomi", "56454");
+	perArr[3] = createPerson("David", "67889");
+	perArr[4] = createPerson("Yosi", "3242234455");
 
-	//if (strcmp(perArr[1]->name, "Moshe"))
-	//	printf("createPerson function is wrong for train3_1 (-3)\n");
-	//if (perArr[2]->numProducts != 0)
-	//	printf("createPerson function is wrong for train3_2 (-3)\n");
-	//if (perArr[4]->products != NULL)
-	//	printf("createPerson function is wrong for train3_2 (-4)\n");
+	if (strcmp(perArr[1]->name, "Moshe"))
+		printf("createPerson function is wrong for train3_1 (-3)\n");
+	if (perArr[2]->numProducts != 0)
+		printf("createPerson function is wrong for train3_2 (-3)\n");
+	if (perArr[4]->products != NULL)
+		printf("createPerson function is wrong for train3_2 (-4)\n");
 
-	//prodArr[0] = createProduct(1, "Sofa", 1500, "mySofa.png", "Great Sofa", perArr[0]);
-	//prodArr[1] = createProduct(2, "Gitar", 1000, "myGitar.png", "Great Gitar", perArr[1]);
-	//prodArr[2] = createProduct(3, "Shoes", 250, "myShoes.jpg", "Great Shoes Ever", perArr[0]);
-	//prodArr[3] = createProduct(4, "Sofa", 1100, "mySofa.png", "A Sofa", perArr[2]);
-	//prodArr[4] = createProduct(5, "Chair", 300, "Chair.png", "4 legs chair", perArr[3]);
-	//prodArr[5] = createProduct(6, "Gitar", 1150, "myGitar.jpg", "Old Gitar", perArr[4]);
-	//prodArr[6] = createProduct(7, "Gitar", 1050, "Gitar.png", "", perArr[2]);
-	//prodArr[7] = createProduct(8, "Sofa", 900, "anotherSofa.png", "", perArr[2]);
+	prodArr[0] = createProduct(1, "Sofa", 1500, "mySofa.png", "Great Sofa", perArr[0]);
+	prodArr[1] = createProduct(2, "Gitar", 1000, "myGitar.png", "Great Gitar", perArr[1]);
+	prodArr[2] = createProduct(3, "Shoes", 250, "myShoes.jpg", "Great Shoes Ever", perArr[0]);
+	prodArr[3] = createProduct(4, "Sofa", 1100, "mySofa.png", "A Sofa", perArr[2]);
+	prodArr[4] = createProduct(5, "Chair", 300, "Chair.png", "4 legs chair", perArr[3]);
+	prodArr[5] = createProduct(6, "Gitar", 1150, "myGitar.jpg", "Old Gitar", perArr[4]);
+	prodArr[6] = createProduct(7, "Gitar", 1050, "Gitar.png", "", perArr[2]);
+	prodArr[7] = createProduct(8, "Sofa", 900, "anotherSofa.png", "", perArr[2]);
 
-	//if (strcmp(prodArr[1]->name, "Gitar"))
-	//	printf("createProduct function is wrong for train3_4 (-3)\n");
-	//if (prodArr[3]->owner != perArr[2])
-	//	printf("createProduct function is wrong for train3_5 (-3)\n");
-	//if (prodArr[7]->price != 900)
-	//	printf("createProduct function is wrong for train3_6 (-4)\n");
-	//
+	if (strcmp(prodArr[1]->name, "Gitar"))
+		printf("createProduct function is wrong for train3_4 (-3)\n");
+	if (prodArr[3]->owner != perArr[2])
+		printf("createProduct function is wrong for train3_5 (-3)\n");
+	if (prodArr[7]->price != 900)
+		printf("createProduct function is wrong for train3_6 (-4)\n");
+	
 
-	//addProductToPerson(perArr[0], prodArr[0]);
-	//addProductToPerson(perArr[1], prodArr[1]);
-	//addProductToPerson(perArr[0], prodArr[2]);
-	//addProductToPerson(perArr[2], prodArr[3]);
-	//addProductToPerson(perArr[3], prodArr[4]);
-	//addProductToPerson(perArr[4], prodArr[5]);
-	//addProductToPerson(perArr[2], prodArr[6]);
-	//addProductToPerson(perArr[2], prodArr[7]);
+	addProductToPerson(perArr[0], prodArr[0]);
+	addProductToPerson(perArr[1], prodArr[1]);
+	addProductToPerson(perArr[0], prodArr[2]);
+	addProductToPerson(perArr[2], prodArr[3]);
+	addProductToPerson(perArr[3], prodArr[4]);
+	addProductToPerson(perArr[4], prodArr[5]);
+	addProductToPerson(perArr[2], prodArr[6]);
+	addProductToPerson(perArr[2], prodArr[7]);
 
-	//if (perArr[0]->numProducts != 2)
-	//	printf("addProductToPerson function is wrong for train3_7 (-3)\n");
-	//if (perArr[2]->products[2]->id != 8)
-	//	printf("addProductToPerson function is wrong for train3_8 (-3)\n");
-	//if (strcmp(perArr[1]->products[0]->pictureLink, "myGitar.png"))
-	//	printf("addProductToPerson function is wrong for train3_9 (-4)\n");
+	if (perArr[0]->numProducts != 2)
+		printf("addProductToPerson function is wrong for train3_7 (-3)\n");
+	if (perArr[2]->products[2]->id != 8)
+		printf("addProductToPerson function is wrong for train3_8 (-3)\n");
+	if (strcmp(perArr[1]->products[0]->pictureLink, "myGitar.png"))
+		printf("addProductToPerson function is wrong for train3_9 (-4)\n");
 
 
 
-	//y2.users = perArr;
-	//y2.numUsers = 5;
-	//retProd = searchProduct(&y2, "Gitar", 1100, &num);
+	y2.users = perArr;
+	y2.numUsers = 5;
+	retProd = searchProduct(&y2, "Gitar", 1100, &num);
 
-	//if (num != 2)
-	//	printf("searchProduct function is wrong for train3_10 (-6)\n");
-	//if (retProd[1]->owner != perArr[2])
-	//	printf("searchProduct function is wrong for train3_11 (-6)\n");
+	if (num != 2)
+		printf("searchProduct function is wrong for train3_10 (-6)\n");
+	if (retProd[1]->owner != perArr[2])
+		printf("searchProduct function is wrong for train3_11 (-6)\n");
 
-	//free(retProd);
-	//retProd = searchProduct(&y2, "Sofa", 2000, &num);
-	//if (num != 3)
-	//	printf("searchProduct function is wrong for train3_12 (-8)\n");
+	free(retProd);
+	retProd = searchProduct(&y2, "Sofa", 2000, &num);
+	if (num != 3)
+		printf("searchProduct function is wrong for train3_12 (-8)\n");
 
 	printf("done");
     return 0;
--- Q3.c
+++ Q3.c
@@ -2,21 +2,76 @@
 
 Person* createPerson(const char* name, const char* phone)
 {
-	return NULL;
+	Person* newPerson = (Person*)malloc(sizeof(Person));
+	newPerson->numProducts = 0;
+	newPerson->products = NULL;
+	strcpy(newPerson->name, name);
+	strcpy(newPerson->phoneNumber, phone);
+	return newPerson;
 }
 
 Product* createProduct(int id, const char* name, int price, const char* pictureLink,
 	const char* description, Person* owner)
 {
-	return NULL;
+	Product* newProduct = (Product*)malloc(sizeof(Product));
+	/*
+	* First, I will allocate memory for the pictureLink and description attributes/fields.
+	*/
+	char* newPictureLink = (char*)malloc(sizeof(char) * (1 + strlen(pictureLink)));
+	char* newDescription = (char*)malloc(sizeof(char)*(1+strlen(description)));
+	strcpy(newPictureLink, pictureLink);
+	strcpy(newDescription, description);
+	/*
+	* now to set the fields/members/attributes
+	*/
+	newProduct->id = id;
+	strcpy(newProduct->name, name);
+	newProduct->price = price;
+	newProduct->description = newDescription;
+	newProduct->pictureLink = newPictureLink;
+	newProduct->owner = owner;
+	return newProduct;
 }
 
 void addProductToPerson(Person* person, Product* product)
 {
+	/*
+	* I assume that product.owner = NULL, and that owner does not have the product
+	* in his product list. The way the questioned was phrased implicates that this
+	* is the most probable situation.
+	*/
+	product->owner = person;//set the product owner to person
+	/*
+	* Now, to reallocate person's products list to its size, +1
+	*/
+	person->products =
+		(Product**)realloc(person->products,
+			sizeof(Product*) * (1+person->numProducts));
+	person->products[person->numProducts] = product;
+	person->numProducts++;
 
-}
+};
 
 Product** searchProduct(Yad2* y2, const char* productName, int limitPrice, int* outNumProducts)
 {
-	return NULL;
+	/*reseting outNumProducts to 0*/
+	*outNumProducts = 0;
+	/* declaring the returned list*/
+	Product** filteredProducts = NULL;
+	/*i iterats over persons*/
+	for (int i = 0; i < y2->numUsers; i++) {
+		Person* currentPerson = y2->users[i];
+		/*j iterates over person[i]'s products*/
+		for (int j = 0; j < currentPerson->numProducts; j++) {
+			Product* currentProduct = currentPerson->products[j];
+			if (strcmp(currentProduct->name, productName) == 0
+				&& currentProduct->price <= limitPrice) {
+				/*first, reallocate filterProducts*/
+				filteredProducts = (Product**)realloc(filteredProducts, (*outNumProducts+1)*sizeof(Product*));
+				filteredProducts[*outNumProducts] = currentProduct;
+				(*outNumProducts)++;
+			}
+		}
+	}
+	return filteredProducts;
 }
