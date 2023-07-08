3c2e1880d014199f4e246453efa076b87e89aeb2
amos zohar <azamoszohar@gmail.com>
2023-07-08 14:55:26 +0300

    finished q1

--- MainTrain.c
+++ MainTrain.c
@@ -54,105 +54,105 @@ int main()
         printf("canEat function is wrong for train1_3 (-9)\n");
 
 
-    //Q2
-	int arr1[] = { 1,2,3,4,5 };
-	int res1 = specialSum(arr1, 5, 5);
+ //   //Q2
+	//int arr1[] = { 1,2,3,4,5 };
+	//int res1 = specialSum(arr1, 5, 5);
 
-	if (count1 < 3)
-		printf("specialSum function is not recursive (-8)\n");
-	else if (res1 != 10)
-	{
-		printf("specialSum function is wrong for train2_1  (-8)\n");
-	}
+	//if (count1 < 3)
+	//	printf("specialSum function is not recursive (-8)\n");
+	//else if (res1 != 10)
+	//{
+	//	printf("specialSum function is wrong for train2_1  (-8)\n");
+	//}
 
-	count1 = 0;
-	int arr2[] = { 1,2,3,-4, 4, 5, 6 };
-	int res2 = specialSum(arr2, 7, 5);
+	//count1 = 0;
+	//int arr2[] = { 1,2,3,-4, 4, 5, 6 };
+	//int res2 = specialSum(arr2, 7, 5);
 
-	if (count1 < 5)
-		printf("specialSum function is not recursive (-9)\n");
-	else if (res2 != 20)
-	{
-		printf("specialSum function is wrong for train2_2  (-9)\n");
-	}
+	//if (count1 < 5)
+	//	printf("specialSum function is not recursive (-9)\n");
+	//else if (res2 != 20)
+	//{
+	//	printf("specialSum function is wrong for train2_2  (-9)\n");
+	//}
 
-	count1 = 0;
-	int arr3[] = { 9,9 };
-	int res3 = specialSum(arr3, 2, 8);
+	//count1 = 0;
+	//int arr3[] = { 9,9 };
+	//int res3 = specialSum(arr3, 2, 8);
 
-	if (res3 != 18)
-	{
-		printf("specialSum function is wrong for train2_3  (-8)\n");
-	}
+	//if (res3 != 18)
+	//{
+	//	printf("specialSum function is wrong for train2_3  (-8)\n");
+	//}
 
-    //Q3
-	Person* perArr[5];
-	Product* prodArr[8];
-	Yad2 y2;
-	Product** retProd;
-	int num;
+ //   //Q3
+	//Person* perArr[5];
+	//Product* prodArr[8];
+	//Yad2 y2;
+	//Product** retProd;
+	//int num;
 
-	perArr[0] = createPerson("David", "32455");
-	perArr[1] = createPerson("Moshe", "232134");
-	perArr[2] = createPerson("Shlomi", "56454");
-	perArr[3] = createPerson("David", "67889");
-	perArr[4] = createPerson("Yosi", "3242234455");
+	//perArr[0] = createPerson("David", "32455");
+	//perArr[1] = createPerson("Moshe", "232134");
+	//perArr[2] = createPerson("Shlomi", "56454");
+	//perArr[3] = createPerson("David", "67889");
+	//perArr[4] = createPerson("Yosi", "3242234455");
 
-	if (strcmp(perArr[1]->name, "Moshe"))
-		printf("createPerson function is wrong for train3_1 (-3)\n");
-	if (perArr[2]->numProducts != 0)
-		printf("createPerson function is wrong for train3_2 (-3)\n");
-	if (perArr[4]->products != NULL)
-		printf("createPerson function is wrong for train3_2 (-4)\n");
+	//if (strcmp(perArr[1]->name, "Moshe"))
+	//	printf("createPerson function is wrong for train3_1 (-3)\n");
+	//if (perArr[2]->numProducts != 0)
+	//	printf("createPerson function is wrong for train3_2 (-3)\n");
+	//if (perArr[4]->products != NULL)
+	//	printf("createPerson function is wrong for train3_2 (-4)\n");
 
-	prodArr[0] = createProduct(1, "Sofa", 1500, "mySofa.png", "Great Sofa", perArr[0]);
-	prodArr[1] = createProduct(2, "Gitar", 1000, "myGitar.png", "Great Gitar", perArr[1]);
-	prodArr[2] = createProduct(3, "Shoes", 250, "myShoes.jpg", "Great Shoes Ever", perArr[0]);
-	prodArr[3] = createProduct(4, "Sofa", 1100, "mySofa.png", "A Sofa", perArr[2]);
-	prodArr[4] = createProduct(5, "Chair", 300, "Chair.png", "4 legs chair", perArr[3]);
-	prodArr[5] = createProduct(6, "Gitar", 1150, "myGitar.jpg", "Old Gitar", perArr[4]);
-	prodArr[6] = createProduct(7, "Gitar", 1050, "Gitar.png", "", perArr[2]);
-	prodArr[7] = createProduct(8, "Sofa", 900, "anotherSofa.png", "", perArr[2]);
+	//prodArr[0] = createProduct(1, "Sofa", 1500, "mySofa.png", "Great Sofa", perArr[0]);
+	//prodArr[1] = createProduct(2, "Gitar", 1000, "myGitar.png", "Great Gitar", perArr[1]);
+	//prodArr[2] = createProduct(3, "Shoes", 250, "myShoes.jpg", "Great Shoes Ever", perArr[0]);
+	//prodArr[3] = createProduct(4, "Sofa", 1100, "mySofa.png", "A Sofa", perArr[2]);
+	//prodArr[4] = createProduct(5, "Chair", 300, "Chair.png", "4 legs chair", perArr[3]);
+	//prodArr[5] = createProduct(6, "Gitar", 1150, "myGitar.jpg", "Old Gitar", perArr[4]);
+	//prodArr[6] = createProduct(7, "Gitar", 1050, "Gitar.png", "", perArr[2]);
+	//prodArr[7] = createProduct(8, "Sofa", 900, "anotherSofa.png", "", perArr[2]);
 
-	if (strcmp(prodArr[1]->name, "Gitar"))
-		printf("createProduct function is wrong for train3_4 (-3)\n");
-	if (prodArr[3]->owner != perArr[2])
-		printf("createProduct function is wrong for train3_5 (-3)\n");
-	if (prodArr[7]->price != 900)
-		printf("createProduct function is wrong for train3_6 (-4)\n");
-	
+	//if (strcmp(prodArr[1]->name, "Gitar"))
+	//	printf("createProduct function is wrong for train3_4 (-3)\n");
+	//if (prodArr[3]->owner != perArr[2])
+	//	printf("createProduct function is wrong for train3_5 (-3)\n");
+	//if (prodArr[7]->price != 900)
+	//	printf("createProduct function is wrong for train3_6 (-4)\n");
+	//
 
-	addProductToPerson(perArr[0], prodArr[0]);
-	addProductToPerson(perArr[1], prodArr[1]);
-	addProductToPerson(perArr[0], prodArr[2]);
-	addProductToPerson(perArr[2], prodArr[3]);
-	addProductToPerson(perArr[3], prodArr[4]);
-	addProductToPerson(perArr[4], prodArr[5]);
-	addProductToPerson(perArr[2], prodArr[6]);
-	addProductToPerson(perArr[2], prodArr[7]);
+	//addProductToPerson(perArr[0], prodArr[0]);
+	//addProductToPerson(perArr[1], prodArr[1]);
+	//addProductToPerson(perArr[0], prodArr[2]);
+	//addProductToPerson(perArr[2], prodArr[3]);
+	//addProductToPerson(perArr[3], prodArr[4]);
+	//addProductToPerson(perArr[4], prodArr[5]);
+	//addProductToPerson(perArr[2], prodArr[6]);
+	//addProductToPerson(perArr[2], prodArr[7]);
 
-	if (perArr[0]->numProducts != 2)
-		printf("addProductToPerson function is wrong for train3_7 (-3)\n");
-	if (perArr[2]->products[2]->id != 8)
-		printf("addProductToPerson function is wrong for train3_8 (-3)\n");
-	if (strcmp(perArr[1]->products[0]->pictureLink, "myGitar.png"))
-		printf("addProductToPerson function is wrong for train3_9 (-4)\n");
+	//if (perArr[0]->numProducts != 2)
+	//	printf("addProductToPerson function is wrong for train3_7 (-3)\n");
+	//if (perArr[2]->products[2]->id != 8)
+	//	printf("addProductToPerson function is wrong for train3_8 (-3)\n");
+	//if (strcmp(perArr[1]->products[0]->pictureLink, "myGitar.png"))
+	//	printf("addProductToPerson function is wrong for train3_9 (-4)\n");
 
 
 
-	y2.users = perArr;
-	y2.numUsers = 5;
-	retProd = searchProduct(&y2, "Gitar", 1100, &num);
+	//y2.users = perArr;
+	//y2.numUsers = 5;
+	//retProd = searchProduct(&y2, "Gitar", 1100, &num);
 
-	if (num != 2)
-		printf("searchProduct function is wrong for train3_10 (-6)\n");
-	if (retProd[1]->owner != perArr[2])
-		printf("searchProduct function is wrong for train3_11 (-6)\n");
+	//if (num != 2)
+	//	printf("searchProduct function is wrong for train3_10 (-6)\n");
+	//if (retProd[1]->owner != perArr[2])
+	//	printf("searchProduct function is wrong for train3_11 (-6)\n");
 
-	free(retProd);
-	retProd = searchProduct(&y2, "Sofa", 2000, &num);
-	if (num != 3)
-		printf("searchProduct function is wrong for train3_12 (-8)\n");
+	//free(retProd);
+	//retProd = searchProduct(&y2, "Sofa", 2000, &num);
+	//if (num != 3)
+	//	printf("searchProduct function is wrong for train3_12 (-8)\n");
 
 	printf("done");
     return 0;
--- Q1.c
+++ Q1.c
@@ -2,5 +2,67 @@
 
 int canEat(char arr[8][8], int x, int y)
 {
-	return -1;
+	if (arr[x][y] == '*')
+		return 0;
+	char p1 = arr[x][y];
+	/*
+	* step 1: check if can eat in directon
+	* of upper-left diagonally:
+	*	1.1. check if destination is inside board.
+	*	1.2. check if destination is free.
+	*	1.3. check if between destination and source there is
+	*		 a cell with value!='*' or the opposite of source value
+	*/
+	if (x - 2 >= 0 && y - 2 >= 0 //1.1
+		&& arr[x-2][y-2] == '*'  //1.2
+		){
+		//1.3
+		if (arr[x - 1][y - 1] != '*'
+			&& arr[x - 1][y - 1] != p1) {
+			return 1;
+		}
+	}
+	/*
+	* Now, I must run a similar check for the 3 remaining directions:
+	*	North East, South East, and South West.
+	*	if none of them returns 1, I shal return 0.
+	*/
+
+	/*
+	* North East
+	*/
+	if (x + 2 < 8 && y - 2 >= 0 //1.1
+		&& arr[x + 2][y - 2] == '*'  //1.2
+		) {
+		//1.3
+		if (arr[x + 1][y - 1] != '*'
+			&& arr[x + 1][y - 1] != p1) {
+			return 1;
+		}
+	}
+	/*
+	* South East
+	*/
+	if (x + 2 < 8 && y + 2 < 8 //1.1
+		&& arr[x + 2][y + 2] == '*'  //1.2
+		) {
+		//1.3
+		if (arr[x + 1][y + 1] != '*'
+			&& arr[x + 1][y + 1] != p1) {
+			return 1;
+		}
+	}
+	/*
+	* South West
+	*/
+	if (x - 2 >= 0 && y + 2 < 8 //1.1
+		&& arr[x - 2][y + 2] == '*'  //1.2
+		) {
+		//1.3
+		if (arr[x - 1][y + 1] != '*'
+			&& arr[x - 1][y + 1] != p1) {
+			return 1;
+		}
+	}
+	return 0;
 }
